bd541f457a67f79a20f62b3d65e357dde552004b
Vivek Rao <vivrao9@gmail.com>
2021-08-10 10:27:36 -0400

    uncommented everything, added a sleep function

--- sudoku.sh
+++ sudoku.sh
@@ -1,14 +1,17 @@
 # script to update the latest sudoku date and time on my portfolio's homepage
-
-
-
+echo $HOME
+# add authentication
+# eval "$(ssh-agent -s)"
+# echo $HOME
+# ssh-add /home/vivrao/.ssh/id_ed25519
+#
 # download latest sudoku sheet
-/usr/bin/wget -oq --no-check-certificate 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSoqnPWcu7Vi2cUAIcH78PFITa-gaVWhQVeEpM4X0Nc4Nd0fHZk98xb221AL3byyU-qAiO4UcZmGrMl/pub?gid=0&single=true&output=csv'
+wget -oq --no-check-certificate 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSoqnPWcu7Vi2cUAIcH78PFITa-gaVWhQVeEpM4X0Nc4Nd0fHZk98xb221AL3byyU-qAiO4UcZmGrMl/pub?gid=0&single=true&output=csv'
 
 # define variables of interest
-latest_date=$(tail -n 1 'pubgid=0&single=true&output=csv' | cut -d , -f 1)
-latest_time=$(tail -n 1 'pubgid=0&single=true&output=csv' | cut -d , -f 2)
-echo $latest_time, $latest_date
+latest_date=$(tail -n 1 'pub@gid=0&single=true&output=csv' | cut -d , -f 1)
+latest_time=$(tail -n 1 'pub@gid=0&single=true&output=csv' | cut -d , -f 2)
+# echo $latest_time, $latest_date
 
 # update the latest_date field
 sed -i "s|<code id='latest_date'>.*</code> and|<code id='latest_date'>$latest_date</code> and|g" index.html
@@ -18,10 +21,10 @@ sed -i "s|<code id='latest_time'>.*</code>|<code id='latest_time'>$latest_time</
 
 # delete temp files
 rm  q
-rm 'pubgid=0&single=true&output=csv'
-
-# push repo to GitHub to update
-git add index.html
-git commit -m "daily sudoku update"
-git push -u origin master
-sleep 5
+rm 'pub@gid=0&single=true&output=csv'
+#
+# # push repo to GitHub to update
+# git add index.html
+# git commit -m "daily sudoku update"
+# git push -u origin master
+sleep 60
